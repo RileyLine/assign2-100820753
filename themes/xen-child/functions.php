8a55c45da440ee61ed2d71a9323c74d89d6dbca7
Riley Line <line.riley@gmail.com>
2021-10-29 15:54:01 -0400

    Created functions.php
    This file is responsible for enqueuing our custom child stylesheet.

---
+++ themes/xen-child/functions.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Functions and definitions
+ *
+ * @link https://developer.wordpress.org/themes/basics/theme-functions/
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty_One
+ * @since Twenty Twenty-One 1.0
+ */
+
+function xenchild_wp_enqueue_styles() {
+    $parenthandle = 'twenty-twenty-one-style';
+    $theme = wp_get_theme();
+    wp_enqueue_style(
+		$parenthandle, 
+		get_template_directory_uri() . '/style.css', 
+        array(),
+        $theme->parent()->get('Version')
+    );
+    wp_enqueue_style(
+		'xen-child-style', 
+		get_stylesheet_uri(),
+        array( $parenthandle ),
+        $theme->get('Version')
+    );
+}
+
+add_action( 'wp_enqueue_scripts', 'xenchild_wp_enqueue_styles' );
