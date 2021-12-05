c71ceb9c91a7860aa7d3a3071659899a2171327c
Riley Line <line.riley@gmail.com>
2021-12-04 19:29:51 -0500

    Updated front page
    Added html structure to front-page file + custom query to bring in 9 "sponsors"

---
+++ themes/xen-child/front-page.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Template for displaying the front page
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty_One
+ * @since Twenty Twenty-One 1.0
+ */
+
+get_header();
+	
+/* Start the Loop */
+while ( have_posts() ) :
+	the_post();
+	get_template_part( 'template-parts/content/content-page' );
+
+	// If comments are open or there is at least one comment, load up the comment template.
+	if ( comments_open() || get_comments_number() ) {
+		comments_template();
+	}
+endwhile; // End of the loop.
+
+$query_args = array(
+	'post_type' => 'post',
+	'post_status' => 'draft',
+	'posts_per_page' => 9
+);
+
+$posts_query = new WP_Query($query_args);
+
+?> 
+	
+	<div class="recentPostsHolder">
+		<h2>Check out some of sponsors</h2>
+		<div class="sponsorsHolder">
+			<?php
+			if ( $posts_query-> have_posts() ) {
+				while ($posts_query-> have_posts()) {
+					$posts_query->the_post(); ?>
+
+					<div class="sponsor">
+						<div class="sponsorImage"><?php the_post_thumbnail(); ?></div>
+						<h3 class="sponsorName"> <?php the_title(); ?></h3>
+						<p class="sponsorLink"> <?php the_content(); ?></p>
+					</div>
+					<?php
+				}
+			}
+			?>
+		</div>
+	</div>
+
+<?php
+
+get_footer();
